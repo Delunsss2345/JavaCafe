00001b3a9bf06639414024c3270dd2cfdae587fa
cauchunhovuive <cuongphat234@gmail.com>
2025-04-17 10:35:17 +0700

    HoaDonCaPheDAO.java

--- src/dao/HoaDonCaPheDAO.java
+++ src/dao/HoaDonCaPheDAO.java
@@ -1,7 +1,6 @@
 //Nguyễn Tuấn Phát
 package dao;
 
-import entities.ChiTietHoaDonCaPhe;
 import entities.HoaDon;
 
 import java.sql.*;
@@ -9,76 +8,80 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import ConnectDB.DatabaseConnection;
-
 public class HoaDonCaPheDAO {
     private Connection conn;
 
     public HoaDonCaPheDAO(Connection conn) {
-		super();
-		this.conn = conn;
-	}
-
-
-	// Thêm hóa đơn vào database
-    public boolean insertHoaDon(HoaDon hd) {
-        String sql = "INSERT INTO HoaDon (maHoaDon, ngayLap, maNhanVien, tenNhanVien, maKhachHang, tenKhachHang, tongTien) " +
-                     "VALUES (?, ?, ?, ?, ?, ?, ?)";
-        try (PreparedStatement ps = conn.prepareStatement(sql)) {
-            ps.setString(1, hd.getMaHoaDon());
-            ps.setTimestamp(2, Timestamp.valueOf(hd.getNgayLap()));
-            ps.setString(3, hd.getMaNhanVien());
-            ps.setString(4, hd.getTenNhanVien());
-            ps.setString(5, hd.getMaKhachHang());
-            ps.setString(6, hd.getTenKhachHang());
-            ps.setDouble(7, hd.getTongTien());
-            return ps.executeUpdate() > 0;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
+        this.conn = conn;
     }
 
-    // Lấy tất cả hóa đơn
-    public List<HoaDon> getAllHoaDon() {
-        List<HoaDon> ds = new ArrayList<>();
-        String sql = "SELECT * FROM HoaDon ORDER BY ngayLap DESC";
-        try (Statement st = conn.createStatement();
-             ResultSet rs = st.executeQuery(sql)) {
+    // Thêm hóa đơn mới
+    public int insertHoaDon(HoaDon hd) {
+        String sql = "INSERT INTO HoaDon (MaDH, NgayTao, TongTien, TienKhachTra, TienThua, MaNV) VALUES (?, ?, ?, ?, ?, ?)";
+        try (PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setInt(1, hd.getMaDH());
+            ps.setTimestamp(2, Timestamp.valueOf(hd.getNgayTao()));
+            ps.setDouble(3, hd.getTongTien());
+            ps.setDouble(4, hd.getTienKhachTra());
+            ps.setDouble(5, hd.getTienThua());
+            ps.setInt(6, hd.getMaNV());
 
-            while (rs.next()) {
-                HoaDon hd = new HoaDon(
-                        rs.getString("maHoaDon"),
-                        rs.getTimestamp("ngayLap").toLocalDateTime(),
-                        rs.getString("maNhanVien"),
-                        rs.getString("tenNhanVien"),
-                        rs.getString("maKhachHang"),
-                        rs.getString("tenKhachHang"),
-                        rs.getDouble("tongTien")
-                );
-                ds.add(hd);
+            int rows = ps.executeUpdate();
+            if (rows > 0) {
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    int maHD = rs.getInt(1);
+                    hd.setMaHD(maHD); // Gán vào đối tượng
+                    return maHD;
+                }
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return ds;
+        return -1;
     }
 
-    // Lấy hóa đơn theo mã
-    public HoaDon getHoaDonByID(String maHoaDon) {
-        String sql = "SELECT * FROM HoaDon WHERE maHoaDon = ?";
+    public List<Object[]> getAllHoaDon() {
+        List<Object[]> list = new ArrayList<>();
+        String sql = """
+            SELECT hd.MaHD, hd.NgayTao, kh.TenKH, hd.TongTien
+            FROM HoaDon hd
+            JOIN DonHang dh ON hd.MaDH = dh.MaDH
+            JOIN KhachHang kh ON dh.MaKH = kh.MaKH
+            ORDER BY hd.NgayTao DESC
+        """;
+
+        try (Statement st = conn.createStatement();
+             ResultSet rs = st.executeQuery(sql)) {
+            while (rs.next()) {
+                Object[] row = new Object[] {
+                    rs.getInt("MaHD"),
+                    rs.getTimestamp("NgayTao").toLocalDateTime(),
+                    rs.getString("TenKH"),
+                    rs.getDouble("TongTien")
+                };
+                list.add(row);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public HoaDon getHoaDonByMaHD(int maHD) {
+        String sql = "SELECT * FROM HoaDon WHERE MaHD = ?";
         try (PreparedStatement ps = conn.prepareStatement(sql)) {
-            ps.setString(1, maHoaDon);
+            ps.setInt(1, maHD);
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
                 return new HoaDon(
-                        rs.getString("maHoaDon"),
-                        rs.getTimestamp("ngayLap").toLocalDateTime(),
-                        rs.getString("maNhanVien"),
-                        rs.getString("tenNhanVien"),
-                        rs.getString("maKhachHang"),
-                        rs.getString("tenKhachHang"),
-                        rs.getDouble("tongTien")
+                    rs.getInt("MaHD"),
+                    rs.getInt("MaDH"),
+                    rs.getTimestamp("NgayTao").toLocalDateTime(),
+                    rs.getDouble("TongTien"),
+                    rs.getDouble("TienKhachTra"),
+                    rs.getDouble("TienThua"),
+                    rs.getInt("MaNV")
                 );
             }
         } catch (SQLException e) {
@@ -87,40 +90,17 @@ public class HoaDonCaPheDAO {
         return null;
     }
 
-    // Xóa hóa đơn theo mã
-    public boolean deleteHoaDon(String maHoaDon) {
-        String sql = "DELETE FROM HoaDon WHERE maHoaDon = ?";
+    public boolean deleteHoaDon(int maHD) {
+        String sql = "DELETE FROM HoaDon WHERE MaHD = ?";
         try (PreparedStatement ps = conn.prepareStatement(sql)) {
-            ps.setString(1, maHoaDon);
+            ps.setInt(1, maHD);
             return ps.executeUpdate() > 0;
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
         }
     }
-    public boolean taoHoaDon(HoaDon hd, List<ChiTietHoaDonCaPhe> dsCT) throws SQLException {
-        Connection con = DatabaseConnection.getInstance().getConnection();
-        try {
-            con.setAutoCommit(false);
+}
 
-            // Insert hóa đơn
-            String sqlHD = "INSERT INTO HoaDon (...) VALUES (...)";
-            // thực hiện insert, lấy ra mã hóa đơn vừa tạo
-
-            // Insert chi tiết hóa đơn
-            for (ChiTietHoaDonCaPhe ct : dsCT) {
-                // gán mã hóa đơn
-                ct.setMaHoaDon(hd.getMaHoaDon());
-                // thực hiện insert chi tiết
-            }
-
-            con.commit();
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            try { con.rollback(); } catch (SQLException ex) {}
-            return false;
-        }
-    }
 
 }
