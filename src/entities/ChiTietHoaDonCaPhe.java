082bbd9b60b19aff3ee177340f4c51655cce55bf
cauchunhovuive <cuongphat234@gmail.com>
2025-04-17 22:13:54 +0700

    ChiTietHoaDonCaPhe.java

--- src/entities/ChiTietHoaDonCaPhe.java
+++ src/entities/ChiTietHoaDonCaPhe.java
@@ -3,80 +3,89 @@ package entities;
 
 public class ChiTietHoaDonCaPhe {
     private String maHoaDon;
-    private String maSanPham;
-    private String tenSanPham;
+    private String maSP;
+    private String tenSP;
     private int soLuong;
     private double donGia;
     private double thanhTien;
 
-    public ChiTietHoaDonCaPhe() {
-    }
-
-    public ChiTietHoaDonCaPhe(String maHoaDon, String maSanPham, String tenSanPham,
-                               int soLuong, double donGia, double thanhTien) {
-        this.maHoaDon = maHoaDon;
-        this.maSanPham = maSanPham;
-        this.tenSanPham = tenSanPham;
+    // ✅ THÊM constructor này:
+    public ChiTietHoaDonCaPhe(String maHD, String maSP, String tenSP, int soLuong, double donGia, double thanhTien) {
+        this.maHoaDon = maHD;
+        this.maSP = maSP;
+        this.tenSP = tenSP;
         this.soLuong = soLuong;
         this.donGia = donGia;
         this.thanhTien = thanhTien;
     }
-
-    // Getters & Setters
+    
+    
     public String getMaHoaDon() {
-        return maHoaDon;
-    }
+		return maHoaDon;
+	}
 
-    public void setMaHoaDon(String maHoaDon) {
-        this.maHoaDon = maHoaDon;
-    }
 
-    public String getMaSanPham() {
-        return maSanPham;
-    }
+	public void setMaHoaDon(String maHoaDon) {
+		this.maHoaDon = maHoaDon;
+	}
 
-    public void setMaSanPham(String maSanPham) {
-        this.maSanPham = maSanPham;
-    }
 
-    public String getTenSanPham() {
-        return tenSanPham;
-    }
+	public String getMaSP() {
+		return maSP;
+	}
 
-    public void setTenSanPham(String tenSanPham) {
-        this.tenSanPham = tenSanPham;
-    }
 
-    public int getSoLuong() {
-        return soLuong;
-    }
+	public void setMaSP(String maSP) {
+		this.maSP = maSP;
+	}
 
-    public void setSoLuong(int soLuong) {
-        this.soLuong = soLuong;
-    }
 
-    public double getDonGia() {
-        return donGia;
-    }
+	public String getTenSP() {
+		return tenSP;
+	}
 
-    public void setDonGia(double donGia) {
-        this.donGia = donGia;
-    }
 
-    public double getThanhTien() {
-        return thanhTien;
-    }
+	public void setTenSP(String tenSP) {
+		this.tenSP = tenSP;
+	}
 
-    public void setThanhTien(double thanhTien) {
-        this.thanhTien = thanhTien;
-    }
 
-    @Override
+	public int getSoLuong() {
+		return soLuong;
+	}
+
+
+	public void setSoLuong(int soLuong) {
+		this.soLuong = soLuong;
+	}
+
+
+	public double getDonGia() {
+		return donGia;
+	}
+
+
+	public void setDonGia(double donGia) {
+		this.donGia = donGia;
+	}
+
+
+	public double getThanhTien() {
+		return thanhTien;
+	}
+
+
+	public void setThanhTien(double thanhTien) {
+		this.thanhTien = thanhTien;
+	}
+
+
+	@Override
     public String toString() {
         return "ChiTietHoaDonCaPhe{" +
                 "maHoaDon='" + maHoaDon + '\'' +
-                ", maSanPham='" + maSanPham + '\'' +
-                ", tenSanPham='" + tenSanPham + '\'' +
+                ", maSanPham='" + maSP + '\'' +
+                ", tenSanPham='" + tenSP + '\'' +
                 ", soLuong=" + soLuong +
                 ", donGia=" + donGia +
                 ", thanhTien=" + thanhTien +
